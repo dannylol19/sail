31bd84e78d33c22a70a590c0a9983b6f6069252b
dannylol19 <vldsdanny719@gmail.com>
2023-01-12 15:29:22 -0500

    Add more registers and change their names

--- vm/registers.h
+++ vm/registers.h
@@ -2,13 +2,16 @@
 #include <cstdint>
 
 enum e_register : uint32_t {
-  a0 = 255,
-  a1,
-  a2,
-  a3,
-  b0,
-  b1,
-  b2,
-  b3,
+  /*! x registers */
+  rax = 255, rbx, rcx, rdx, rex, rfx, rgx, rhx, rix, rjx, rkx, rlx, rmx, rnx, rox, rpx, rqx, rrx, rsx, rtx, rux, rvx, rwx, rxx, ryx, rzx,
+
+  /* if we need more registers
+  // y registers: \\
+  ray, rby, rcy, rdy, rey, rfy, rgy, rhy, riy, rjy, rky, rly, rmy, rny, roy, rpy, rqy, rry, rsy, rty, ruy, rvy, rwy, rxy, ryy, rzy,
+
+  // z registers: \\
+  raz, rbz, rcz, rdz, rez, rfz, rgz, rhz, riz, rjz, rkz, rlz, rmz, rnz, roz, rpz, rqz, rrz, rsz, rtz, ruz, rvz, rwz, rxz, ryz, rzz,
+  */
+
   REGISTERS_END
 };
